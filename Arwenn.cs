2666706a7271b1ea3e20fe1e04ca6f4e74355db9
Louphie42 <louphie@outlook.fr>
2023-01-17 10:54:21 +0100

    Create Arwenn.cs

---
+++ Arwenn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitGitHub
+{
+    internal class Arwenn
+    {
+        public void MaMethode()
+        {
+            Console.WriteLine("Ceci est la methode de Arwenn");
+        }
+    }
+}
