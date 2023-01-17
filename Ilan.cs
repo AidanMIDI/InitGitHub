48145972d782741a64933f09698998eac1f5e05a
AidanMIDI <ilan.baurain@e-artsup.net>
2023-01-17 10:25:34 +0100

    Class Ilan

---
+++ Ilan.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitGitHub
+{
+    internal class Class1
+    {
+    }
+}
